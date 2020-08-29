30a6254aa17f42e0b0e1a9a03332fe42ed62de8f
Sean Coonce <cooncesean@gmail.com>
2020-08-29 16:46:46 -0700

    remove docusuarus files

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/Users/coonce/dev/handbook-test/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/coonce/dev/handbook-test/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/coonce/dev/handbook-test/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/coonce/dev/handbook-test/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/coonce/dev/handbook-test/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
-  require("/Users/coonce/dev/handbook-test/src/css/custom.css"),
-];
